e58072fe6766b5632a09ecd8c5dc50d657fff9e6
acetop999777 <acetop97@gmail.com>
2026-01-13 07:37:32 +0000

    backup: scripts/make/compose sync + ops hardening

--- p922.sh
+++
@@ -1,8 +0,0 @@
-docker inspect pc_inv_server | python3 - <<'PY'
-import json,sys
-o=json.load(sys.stdin)[0]
-print("status=", o.get("State",{}).get("Status"),
-      "exit=", o.get("State",{}).get("ExitCode"),
-      "restarts=", o.get("RestartCount",0),
-      "oom=", o.get("State",{}).get("OOMKilled"))
-PY
