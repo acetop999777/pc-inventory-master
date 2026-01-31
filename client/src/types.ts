629871189936bff8808393c9e976f5d75d05fb68
acetop999777 <acetop97@gmail.com>
2026-01-31 05:02:52 +0000

    remove unused global types file

--- client/src/types.ts
+++
@@ -1,4 +0,0 @@
-import type { ClientEntity } from './domain/client/client.types';
-import type { InventoryItem } from './domain/inventory/inventory.types';
-
-export type { InventoryItem } from './domain/inventory/inventory.types';
